bab399ec775ff1e6c86e9e7c054115a75d96172a
yangdongji <12418339@qq.com>
2016-08-31 16:45:06 +0800

    python 并行化爬虫爬取时间比较

---
+++ lala.py
@@ -0,0 +1,27 @@
+from multiprocessing.dummy import Pool as ThreadPool
+import  requests
+import	time
+
+def getsource(url):
+	html = requests.get(url)
+
+urls = []
+
+for i in range(1,100):
+	newpage = 'http://tieba.badiu.com/p/3522395718?pn=' + str(i)
+	urls.append(newpage)
+
+time1 = time.time()
+for i in urls:
+	print(i)
+	getsource(i)
+time2 = time.time()
+print(u'单线程耗时'+str(time2-time1))
+
+pool = ThreadPool(4)
+time3 = time.time()
+results = pool.map(getsource,urls)
+pool.close()
+pool.join()
+time4 = time.time()
+print(u'并行耗时'+str(time4-time3))
