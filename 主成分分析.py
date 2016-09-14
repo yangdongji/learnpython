540395b5d1afe91386b125c527879004aa9d3997
Alex Young <12418339@qq.com>
2016-09-14 14:22:18 +0800

    Create 主成分分析.py

---
+++ 主成分分析.py
@@ -0,0 +1,72 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+def loadDataSet(fileName,delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    datArr = [map(float,line) for line in stringArr]
+    return mat(datArr)
+
+def pca(dataMat,topNfeat=9999999):
+    meanVals = mean(dataMat,axis=0)
+    meanRemoved = dataMat - meanVals #减去平均值
+    covMat = cov(meanRemoved,rowvar= 0)
+    eigVals,eigVects = linalg.eig(mat(covMat))
+    eigValInd = argsort(eigVals)  #计算特征值和特征向量
+    eigValInd = eigValInd[:-(topNfeat+1):-1] #保留最大的前K个特征量
+    redEigVects = eigVects[:,eigValInd] #对应的特征向量
+    lowDDataMat = meanRemoved*redEigVects #重构数据，用于调试
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat,reconMat
+
+def plotBestFit(dataSet1,dataSet2):
+    dataArr1 = array(dataSet1)
+    dataArr2 = array(dataSet2)
+    n = shape(dataArr1)[0]
+    n1 = shape(dataArr2)[0]
+    xcord1 = [];ycord1=[]
+    xcord2 = [];ycord2=[]
+    xcord3 = [];ycord3=[]
+    j = 0
+    for i in range(n):
+        xcord1.append(dataArr1[i,0]);ycord1.append(dataArr1[i,1])
+        xcord2.append(dataArr2[i,0]);ycord2.append(dataArr2[i,1])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+if __name__ == '__main__':
+    mata = loadDataSet('testSet.txt')
+    a,b= pca(mata,2)
+    plotBestFit(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
